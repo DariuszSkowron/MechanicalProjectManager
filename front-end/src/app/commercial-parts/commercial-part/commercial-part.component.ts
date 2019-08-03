6cd30cce01f1284204b74bd1ac82a272aa4b5e7f
Dariusz Skowron <dariusz.skowron@gmail.com>
2019-08-03 23:53:20 +0200

    commercial parts changes

--- front-end/src/app/commercial-parts/commercial-part/commercial-part.component.ts
+++ front-end/src/app/commercial-parts/commercial-part/commercial-part.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {CommercialPart} from '../model/commercial-part';
 
 @Component({
   selector: 'commercial-part',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./commercial-part.component.scss']
 })
 export class CommercialPartComponent implements OnInit {
+  @Input() commercialPart: CommercialPart;
+  @Output() commercialPartUpdated: EventEmitter<CommercialPart> = new EventEmitter<CommercialPart>();
+  @Output() commercialPartDeleted: EventEmitter<CommercialPart> = new EventEmitter<CommercialPart>();
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
+  updateCommercialPart() {
+    this.commercialPartUpdated.emit(this.commercialPart);
+  }
+
+  deleteCommercialPart() {
+    this.commercialPartDeleted.emit(this.commercialPart);
+  }
 }
