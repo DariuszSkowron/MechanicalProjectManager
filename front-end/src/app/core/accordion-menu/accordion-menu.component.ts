a063b8ae70062dab44209ff5bc82d9e806d4add3
Dariusz Skowron <dariusz.skowron@gmail.com>
2019-08-11 20:14:01 +0200

    added accordion-menu

---
+++ front-end/src/app/core/accordion-menu/accordion-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'accordion-menu',
+  templateUrl: './accordion-menu.component.html',
+  styleUrls: ['./accordion-menu.component.scss']
+})
+export class AccordionMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
