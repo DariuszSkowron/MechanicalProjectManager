619a73c373e2606d7ede81af9caf357debffc797
Dariusz Skowron <dariusz.skowron@gmail.com>
2019-08-12 23:58:04 +0200

    need to add angular transition animation

--- front-end/src/app/core/accordion-menu/accordion-menu.component.ts
+++ front-end/src/app/core/accordion-menu/accordion-menu.component.ts
@@ -1,16 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup} from '@angular/forms';
 import { NgbDropdown} from '@ng-bootstrap/ng-bootstrap';
+import {animate, transition} from '@angular/animations';
 
 @Component({
   selector: 'accordion-menu',
   templateUrl: './accordion-menu.component.html',
-  styleUrls: ['./accordion-menu.component.scss']
+  styleUrls: ['./accordion-menu.component.scss'],
+  animations: [
+    transition('open => closed', [
+      animate('1s')
+    ]),
+  ]
+
 })
 
 
 export class AccordionMenuComponent implements OnInit {
   public isCollapsed = false;
+
   ngOnInit() {
   }
 }
