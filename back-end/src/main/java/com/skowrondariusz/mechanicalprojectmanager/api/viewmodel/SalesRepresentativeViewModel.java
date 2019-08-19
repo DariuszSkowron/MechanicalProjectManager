9595ce74fce11fef7cec89c5089394e3db4b1882
Dariusz Skowron <dariusz.skowron@gmail.com>
2019-08-19 23:28:01 +0200

    added view models to manufacturer and sales representative

--- back-end/src/main/java/com/skowrondariusz/mechanicalprojectmanager/api/viewmodel/SalesRepresentativeViewModel.java
+++ back-end/src/main/java/com/skowrondariusz/mechanicalprojectmanager/api/viewmodel/SalesRepresentativeViewModel.java
@@ -1,4 +1,50 @@
 package com.skowrondariusz.mechanicalprojectmanager.api.viewmodel;
 
 public class SalesRepresentativeViewModel {
+
+    private Long id;
+    private String name;
+    private String lastName;
+    private String email;
+    private String manufacturer;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
 }
