40f78af560870a48edfd530476a188c660fabc18
Dariusz Skowron <dariusz.skowron@gmail.com>
2019-09-04 23:50:22 +0200

    parts lists and project relations

--- back-end/src/main/java/com/skowrondariusz/mechanicalprojectmanager/repository/CommercialPartRepository.java
+++ back-end/src/main/java/com/skowrondariusz/mechanicalprojectmanager/repository/CommercialPartRepository.java
@@ -1,6 +1,7 @@
 package com.skowrondariusz.mechanicalprojectmanager.repository;
 
 import com.skowrondariusz.mechanicalprojectmanager.model.CommercialPart;
+import com.skowrondariusz.mechanicalprojectmanager.model.Manufacturer;
 import com.skowrondariusz.mechanicalprojectmanager.model.PartsOrder;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,4 +11,5 @@ import java.util.List;
 @Repository
 public interface CommercialPartRepository extends JpaRepository<CommercialPart, Long> {
     List<CommercialPart> findAllByPartsOrder(PartsOrder partsOrder);
+    List<CommercialPart> findAllByManufacturer(Manufacturer manufacturer);
 }
