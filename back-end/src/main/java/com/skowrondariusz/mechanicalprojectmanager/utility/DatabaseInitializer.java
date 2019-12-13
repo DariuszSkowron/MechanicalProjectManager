f8d2a941f59f95a69b570e887a8b3de32c4d9328
Dariusz Skowron <dariusz.skowron@gmail.com>
2019-12-13 21:50:22 +0100

    inquiry sender changes

--- back-end/src/main/java/com/skowrondariusz/mechanicalprojectmanager/utility/DatabaseInitializer.java
+++ back-end/src/main/java/com/skowrondariusz/mechanicalprojectmanager/utility/DatabaseInitializer.java
@@ -1,7 +1,9 @@
 package com.skowrondariusz.mechanicalprojectmanager.utility;
 
 import com.skowrondariusz.mechanicalprojectmanager.model.Manufacturer;
+import com.skowrondariusz.mechanicalprojectmanager.model.SalesRepresentative;
 import com.skowrondariusz.mechanicalprojectmanager.repository.ManufacturerRepository;
+import com.skowrondariusz.mechanicalprojectmanager.repository.SalesRepresentativeRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
@@ -11,15 +13,21 @@ import org.springframework.stereotype.Component;
 public class DatabaseInitializer implements CommandLineRunner {
 
     private ManufacturerRepository manufacturerRepository;
+    private SalesRepresentativeRepository salesRepresentativeRepository;
 
-    public DatabaseInitializer(ManufacturerRepository manufacturerRepository){
+    public DatabaseInitializer(ManufacturerRepository manufacturerRepository, SalesRepresentativeRepository salesRepresentativeRepository){
         this.manufacturerRepository = manufacturerRepository;
+        this.salesRepresentativeRepository = salesRepresentativeRepository;
     }
 
     @Override
     public void run(String... args){
         var defaultManufacturer = new Manufacturer("UNASSIGNED");
         this.manufacturerRepository.save(defaultManufacturer);
+
+        var defaultSalesRep = new SalesRepresentative("Test", "Test", "test@gmail.com", manufacturerRepository.getManufacturerById(1));
+        this.salesRepresentativeRepository.save(defaultSalesRep);
+
 
 
         System.out.println("BASIC DATABASE INITIALIZED");
