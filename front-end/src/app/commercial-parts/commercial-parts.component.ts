a1850f9af6f317edba6b5604a0c68d29e498bd21
Dariusz Skowron <dariusz.skowron@gmail.com>
2020-01-11 22:54:43 +0100

    commercial parts fixes

--- front-end/src/app/commercial-parts/commercial-parts.component.ts
+++ front-end/src/app/commercial-parts/commercial-parts.component.ts
@@ -57,10 +57,16 @@ export class CommercialPartsComponent implements OnInit, AfterViewInit {
   //   }, 200);
   // }
 
-  // onPageChangeTest() {
-  //   this.currentItemsToShow = this.commercialParts.slice(this.x, this.y + 1);
-  // }
-  //
+  onPageChangeTest() {
+    this.currentItemsToShow = this.commercialParts.slice(this.x, this.y + 1);
+  }
+
+  onPageChangeTest2(numberOfDeletions: number) {
+    this.currentItemsToShow = this.commercialParts.slice(this.x, this.y - numberOfDeletions);
+  }
+
+
+
   onPageChange($event) {
     this.x = $event.pageIndex * $event.pageSize;
     this.y = $event.pageIndex * $event.pageSize + $event.pageSize;
@@ -69,7 +75,7 @@ export class CommercialPartsComponent implements OnInit, AfterViewInit {
   }
 
   getAllSelected() {
-    this.selectedCommercialParts = this.commercialParts
+    this.selectedCommercialParts = this.currentItemsToShow
       .filter(commercial => commercial.checked === true && commercial.invoiceId == null);
   }
 
@@ -103,6 +109,7 @@ export class CommercialPartsComponent implements OnInit, AfterViewInit {
     this.projectService.getAllCommercialParts().subscribe(
       res => {
         this.commercialParts = res;
+        this.currentItemsToShow = this.commercialParts;
       },
       err => {
         alert('While downloading the parts orders occurred an error');
@@ -185,12 +192,12 @@ export class CommercialPartsComponent implements OnInit, AfterViewInit {
 
   deleteSelectedCommercialParts() {
     if (confirm('Do you want to delete selected parts?')) {
-      this.projectService.deleteSelectedCommercialParts(this.commercialParts
+      const kupa: number = this.currentItemsToShow.filter((commercial => commercial.checked === true)).length;
+      this.projectService.deleteSelectedCommercialParts(this.currentItemsToShow
         .filter(commercial => commercial.checked === true).map(commercial => commercial.id)).subscribe(
         res => {
           console.log(res);
-          this.getAllCommercialParts();
-          // this.initPaginator();
+          this.selectPartsOrder2();
         },
         err => {
           alert('Failed to delete selected parts');
@@ -232,6 +239,18 @@ export class CommercialPartsComponent implements OnInit, AfterViewInit {
   selectPartsOrder(partsOrder: PartsOrder) {
     this.selectedPartsOrder = partsOrder;
     this.projectService.getCommercialPartsByPartsOrder(partsOrder.id).subscribe(
+      res => {
+        this.commercialParts = res;
+        this.currentItemsToShow = this.commercialParts;
+      },
+      err => {
+        alert('An error has occurred while fetching the parts');
+      }
+    );
+  }
+
+  selectPartsOrder2() {
+    this.projectService.getCommercialPartsByPartsOrder(this.selectedPartsOrder.id).subscribe(
       res => {
         this.commercialParts = res;
         this.currentItemsToShow = this.commercialParts;
